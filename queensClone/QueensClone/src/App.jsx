09c9974c964e83412ff0309e071ab86797d82ede
Munashe Mukweya <munashemukweya2022@gmail.com>
2024-11-10 20:26:37 -0600

    Added a hint button

---
+++ queensClone/QueensClone/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App(){
+  return <div id='game'>
+  <div id='board'>
+    <span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span><span id="button"></span>
+    </div>
+  </div>
+}
